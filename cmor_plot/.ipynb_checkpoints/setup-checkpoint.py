ededbf3bc6fa773299459e962220fb16613da61a
Maxwell Elling <70956803+Mte2112@users.noreply.github.com>
2023-08-07 11:49:03 -0400

    Delete cmor_plot/.ipynb_checkpoints directory

--- cmor_plot/.ipynb_checkpoints/setup-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='dist_alx',
-      version='0.0',
-      description='Gaussian and Binomial distributions',
-      packages=['dist_alx'],
-      author_email='mihajlovic.aleksa@gmail.com',
-      zip_safe=False)
