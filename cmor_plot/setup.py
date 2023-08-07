f3c883dd256bff08ca7516cb662acf94f3d2c168
Maxwell Elling <70956803+Mte2112@users.noreply.github.com>
2023-08-07 11:50:56 -0400

    versioning

--- cmor_plot/setup.py
+++ cmor_plot/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cmor_plot',
-      version='0.0',
+      version='1.0',
       description='Functions for cmor_plotcheck',
       packages=['cmor_plot'],
       authors='Maxwell Elling: maxwellelling@gmail.com, Alex Herron: ajoherron@gmail.com',
